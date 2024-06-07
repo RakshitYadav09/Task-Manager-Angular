1750fbb8cd7fe5bd9145fbb84c3faf968f1fc592
Rakshit <rakshityadav411@gmail.com>
2024-06-07 11:15:10 +0530

    Add Tasks Working

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { Title } from '@angular/platform-browser';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
     selector: 'app-tasks',
     standalone: true,
     templateUrl: './tasks.component.html',
     styleUrl: './tasks.component.css',
-    imports: [TaskComponent]
+    imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name!: string;
+  isAddingTaksk=false;
   tasks = [
     {
       id:'t1',
@@ -39,4 +41,13 @@ export class TasksComponent {
   get selectedUserTasks(){
     return this.tasks.filter((task) => task.userId === this.userId);
   }
+
+  onCompleteTask(id: string){
+    this.tasks=this.tasks.filter((task)=> task.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTaksk=true;
+  }
 }
+
